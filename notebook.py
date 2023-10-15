5ce50e10f078c58d59a184cebc99e5e319355efb
Aftermath112 <vyanchenko.rpu@gmail.com>
2023-10-15 21:13:21 +0300

    Added functions: adding tags, adding notes, deleting notes, searching by notes/tags/names, save/load

--- notebook.py
+++ notebook.py
@@ -1,16 +1,26 @@
+import pickle
+
 class Note:
     def __init__(self,  name):
         self.text = ''
         self.name = name
+        self.tags = []
 
     def edit_text(self, text):
         self.text = text
 
+    def add_tag(self, tag):
+        self.tags.append(tag)
+
+    def remove_tag(self, tag):
+        if tag in self.tags:
+            self.tags.remove(tag)
 
 class Notebook:
     def __init__(self):
         self.notes = []
         self.tag_dictionary = {}
+        self.load_data ()
 
     def add_tags(self, note, tags):
         for tag in tags:
@@ -46,4 +56,17 @@ class Notebook:
 
     def search_notes_by_text(self, query):
         return [note for note in self.notes if query in note.text]
-#to be continued
+
+    def load_data(self):
+        try:
+            with open('notebook.pkl', 'rb') as file:
+                loaded_notebook = pickle.load(file)
+                self.notes = loaded_notebook.notes
+                self.tag_dictionary = loaded_notebook.tag_dictionary
+        except FileNotFoundError:
+            self.notes = []
+            self.tag_dictionary = {}
+
+    def save_data(self):
+        with open('notebook.pkl', 'wb') as file:
+            pickle.dump(self, file)
