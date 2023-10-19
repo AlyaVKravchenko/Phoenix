c8d7b442e881240b04f7620c8322284c8db93d77
AlyaVKravchenko <alya17@ukr.net>
2023-10-19 13:39:05 +0300

    Final

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(name='Phoenix',
       version='0.0.1',
       description='Your personal assistant',
       url='https://github.com/AlyaVKravchenko/Phoenix',
       author='project-group-12',
-      packages=find_packages(),
+      packages=find_namespace_packages(),
       license='MIT',
       entry_points={"console_scripts":["Ineedhelp = main:main"]},
       install_requires=["contacts", "notebook", "sorter"],
