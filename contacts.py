388c73536e03288b57af10229080a1887d223883
Vandergraf87 <30665094+Vandergraf87@users.noreply.github.com>
2023-10-19 13:44:16 +0300

    Revert "Revert "Dev alya fix 14""

--- contacts.py
+++ contacts.py
@@ -242,12 +242,15 @@ class AddressBook(UserDict):
         self.save_data() 
 
     def search(self, query):
-        found = False
+        matching_contacts = []
         for name, record in self.data.items():
             if query in name or any(query in phone.value for phone in record.phones):
-                print(f"Name: {name}, Phone: {[phone._value for phone in record.phones]}")
-                found = True
-        if not found:
+                matching_contacts.append((name, [phone._value for phone in record.phones]))
+
+        if matching_contacts:
+            for name, phones in matching_contacts:
+                print(f"Name: {name}, Phone: {phones}")
+        else:
             print("No matching contacts found.")
 
     def delete_contact(self, name):
